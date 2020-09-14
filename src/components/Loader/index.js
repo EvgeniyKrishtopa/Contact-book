8611848bdcbf5baf25b6e3712b34717a21a6ef23
EvgeniyKrishtopa <evgeniykrishtopa@gmail.com>
2020-09-14 13:54:55 +0300

    fix common styles and loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import styles from './styles.module.scss';
-import MaterialIcon from 'material-icons-react';
-
-const preloader = (
-  <div className={styles.ldsRipple}>
-    <div></div>
-    <div></div>
-  </div>
-);
 
 const Loader = () => {
-  return <MaterialIcon icon="dashboard" preloader={preloader} />;
-}
+  return (
+    <div className="page-center">
+      <div className={styles.ldsDualRing}></div>
+    </div>
+  );
+};
 
 export default Loader;
